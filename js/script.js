16d1265693bdcd926e0706f777cc972bd223d1ff
Engie Ruge <engnatruve@gmail.com>
2016-03-04 11:10:18 -0500

    Ejercicio ajax
    jquery + ajax

--- js/script.js
+++ js/script.js
@@ -3,9 +3,33 @@ $(document).ready(function(){
     $("section").fadeIn(2000, function(){
 
     	 $("textarea").focus();
-    });
+    }); 
 
-   
-    
+	$("button").click(function(){
+		llamarada();
+	});    
 });
 
+function llamarada(){
+	$.ajax({
+	    url: "https://api.spotify.com/v1/search",
+	    data: {
+	      type: 'album',
+	      q : 'The Rolling Stones'
+	    }
+	})
+    .done(function(data) {
+		// do things w/ oData (JS object)
+		var $albumArtista = $('#Album');
+		$.each(data.albums.items, function(key, value){
+			html = "<article><p><strong>Nombre del album:</strong> "+value.name+"</p><img src='"+value.images[0].url+"'></article>'";
+			$albumArtista.append(html);
+			console.log(value.name);
+		});
+		console.log(data.album.items );
+	});
+}
+
+
+
+   
