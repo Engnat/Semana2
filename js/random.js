f7445f38322d7f25a2def35a81b9c8af4d7ff224
Engie Ruge <engnatruve@gmail.com>
2016-03-09 06:23:00 -0500

    ejercicio numeros random
    correccion

--- js/random.js
+++ js/random.js
@@ -1,25 +1,24 @@
 var numeros = [];
-var cumple ;
+//var cumple ;
 
 	function obtenerNumR() {
 
 		
-		var valorNumeros = document.getElementById("numeros").value;
-		var rangoIni = document.getElementById("ini").value;
-		var rangoFin = document.getElementById("fin").value;
-		numeros = [valorNumeros];
-		console.log("Hola estos son los numeros", valorNumeros , rangoIni, rangoFin);
+		var valorNumeros = Number(document.getElementById("numeros").value);
+		var rangoIni = Number(document.getElementById("ini").value);
+		var rangoFin = Number(document.getElementById("fin").value);
+		console.log("Hola estos son los numeros", numeros);
 		
 
-		if(rangoIni == 0){
+		if(rangoIni === 0){
 			alert("El rango inicial no puede ser 0");
 		}else{
 		
 			if(rangoFin - rangoIni >= valorNumeros){
 
 				console.log("Siga");
-				aleatoria(valorNumeros, rangoIni, rangoFin);
-
+				var numeros = getNumbers(valorNumeros ,rangoIni, rangoFin);
+				document.getElementById("arreglo").innerHTML=numeros;
 			}else{
 				console.log('bruto');
 				alert("Revise el rango");
@@ -28,50 +27,29 @@ var cumple ;
 
 	}
 
-	
-
-	function aleatoria(num, min, max){
+	function getNumbers(num, min, max){
 		var j = 0;
-		var n;
-		
+		var numeros=[];	
 		while(j<num){
 			console.log("Entre al while", j);
+			function getRandomNum(){
 
-			n = Math.round(Math.random() * min) + (max - min);
-			for (var i = 0; numeros.length > i; i++) {
-
-				if (numeros.indexOf(n) != -1 ) {
-
-					console.log("numero repetido",n);
-					n = Math.round(Math.random() * min) + (max - min);
-					
-					
-					
-				}else{
-
-					numeros.push(n);
-					console.log("el numero es",n, "con numero", j);
-					break;
-				} 
+				var randomNumber = Math.floor(Math.random() * max) + min;
+				if (numeros.indexOf(randomNumber) != -1 ){
+					return getRandomNum();
+				}
+				return randomNumber;
 			}
 			
+			numeros.push(getRandomNum());
 			j++;
-			
-
-
 		}
-
-		for (var t = 1; numeros.length > t; t++) {
-
-			console.log("valor", numeros[t], "con posicion", t);
-	
-			
-		}
-				
+		console.log(numeros);
+		return numeros;		
 	}
 
 	
 	/*como hago para retomar el if, ya que return lo detiene y break rompe el ciclo?
-	  siempre generada solo 2 numeros el rango final y otro numero o el numero de elementos que se mostraran
+	  siempre generaba solo 2 numeros el rango final y otro numero o el numero de elementos que se mostraran
 	  rango repite(10-1-11)
 	*/
